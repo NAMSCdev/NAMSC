5581f0e1aa8afb90ad1b921c7705ae302b0fb51d
LightTab2 <azorro12@gmail.com>
2023-01-14 01:25:36 +0100

    Old. But compiles. Lacks feataures (#11)

--- NAMSC_engine/main.cpp
+++ NAMSC_engine/main.cpp
@@ -1,10 +1,10 @@
-#include "mainWindow.h"
+#include "NAMSC_engine.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    NAMSC_engine w;
     w.show();
     return a.exec();
 }
---
+++ NAMSC_lib/src/Novel/Widget/SceneWidget.cpp
@@ -0,0 +1,110 @@
+﻿#include "Novel/Widget/SceneWidget.h"
+
+#include <QResizeEvent>
+
+SceneWidget::SceneWidget(QWidget* parent)
+	: QGraphicsView(parent)
+{
+	setCacheMode(QGraphicsView::CacheBackground);
+	setScene(new QGraphicsScene());
+}
+
+void SceneWidget::switchToPreview()
+{
+	preview_ = true;
+	for (SceneryObjectWidget* sceneryObjectWidget : sceneryObjectWidgets_)
+		sceneryObjectWidget->switchToPreview();
+}
+
+void SceneWidget::switchToDisplay()
+{
+	preview_ = false;
+	for (SceneryObjectWidget* sceneryObjectWidget : sceneryObjectWidgets_)
+		sceneryObjectWidget->switchToDisplay();
+}
+
+const std::vector<SceneryObjectWidget*>* SceneWidget::getSceneryObjectWidgets() const noexcept
+{
+	return &sceneryObjectWidgets_;
+}
+
+const SceneryObjectWidget* SceneWidget::getSceneryObjectWidget(size_t index) const noexcept
+{
+	return sceneryObjectWidgets_[index];
+}
+
+SceneryObjectWidget* SceneWidget::getSceneryObjectWidget(size_t index) noexcept
+{
+	return sceneryObjectWidgets_[index];
+}
+
+void SceneWidget::addSceneryObjectWidget(const SceneryObject& sceneryObject) noexcept
+{
+	if (!sceneryObject.getAssetImage())
+		return;
+	SceneryObjectWidget* sceneryObjectWidget = new SceneryObjectWidget(sceneryObject, preview_);
+
+	sceneryObjectWidgets_.push_back(sceneryObjectWidget);
+	scene()->addItem(sceneryObjectWidget);
+	sceneryObjectWidget->setZValue(scene()->items().size() - 1);
+}
+
+bool SceneWidget::insertSceneryObjectWidget(size_t index, const SceneryObject& sceneryObject)
+{
+	if (index > sceneryObjectWidgets_.size())
+	{
+		qCritical() << NovelLib::ErrorType::General << "Tried to insert past \"sceneryObjectWidgets_\" size";
+		return false;
+	}
+	SceneryObjectWidget* sceneryObjectWidget = new SceneryObjectWidget(sceneryObject, preview_);
+
+	scene()->addItem(sceneryObjectWidget);
+	sceneryObjectWidgets_.insert(sceneryObjectWidgets_.begin() + index, std::move(sceneryObjectWidget));
+	//Correct Z-Values after inserting a new element
+	for (int i = 0; i != sceneryObjectWidgets_.size(); ++i)
+		(*(sceneryObjectWidgets_.begin() + i))->setZValue(i);
+	return true;
+}
+
+bool SceneWidget::removeSceneryObjectWidget(size_t index)
+{
+	if (index >= sceneryObjectWidgets_.size())
+	{
+		qCritical() << NovelLib::ErrorType::General << "Tried to remove past \"sceneryObjectWidgets_\" size";
+		return false;
+	}
+	sceneryObjectWidgets_.erase(sceneryObjectWidgets_.begin() + index);
+	//Removing an item doesn't need to correct Z-Values
+	return true;
+}
+
+void SceneWidget::clearSceneryObjectWidgets() noexcept
+{
+	sceneryObjectWidgets_.clear();
+}
+
+void SceneWidget::resizeEvent(QResizeEvent* event)
+{
+	QGraphicsView::resizeEvent(event);
+	//No Image resize needed, since it will be drawn (where it will be resized) and then cached
+	//QImage img = scene()->backgroundBrush().textureImage();
+	//if (!img.isNull())
+	//	changeBackground(&img);
+}
+
+void SceneWidget::drawBackground(QPainter* painter, const QRectF& rect)
+{
+	QImage img = scene()->backgroundBrush().textureImage();
+	if (!img.isNull())
+	{
+		QSize size = rect.size().grownBy(QMarginsF(0.0, 0.0, 0.49999, 0.49999)).toSize();
+		painter->drawImage(QPoint(-size.width()/2, -size.height()/2), img.scaled(size, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+	}
+}
+
+void SceneWidget::changeBackground(const QImage* img)
+{
+	//No resize needed, since it is cached
+	QBrush brush(*img/*->scaled(size())*/);
+	scene()->setBackgroundBrush(brush);
+}
