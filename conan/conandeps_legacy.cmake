d7390f28393d814b1b531bd38995760bc686b119
LightTab2 <azorro12@gmail.com>
2025-01-31 18:36:33 +0100

    Remove build artifacts

--- conan/conandeps_legacy.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(Boost)
-find_package(Microsoft.GSL)
-
-set(CONANDEPS_LEGACY  boost::boost  Microsoft.GSL::GSL )
